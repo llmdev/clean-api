99a7bec28bffbc09c5a7240c18369fdcad7e9ef7
Lucas Lopes de Moura <lucaslopes.dev@gmail.com>
2020-07-01 00:03:42 -0300

    feat: return server error on throw new exception signup controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,3 +1,4 @@
+import { ServerError } from './../errors/server-error'
 import { InvalidParamError } from './../errors/invalid-param-error'
 import { EmailValidator } from './../protocols/emailValidator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
@@ -12,31 +13,38 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirm']
-    const errors = requiredFields.map(field => {
-      if (!httpRequest.body[field]) {
-        return new MissingParamError(field)
-      }
-    }).filter(f => f !== undefined)
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirm']
+      const errors = requiredFields.map(field => {
+        if (!httpRequest.body[field]) {
+          return new MissingParamError(field)
+        }
+      }).filter(f => f !== undefined)
 
-    if (errors.length) {
-      return {
-        statusCode: 400,
-        body: {
-          error: errors
+      if (errors.length) {
+        return {
+          statusCode: 400,
+          body: {
+            error: errors
+          }
         }
       }
-    }
-    const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
 
-    if (!emailIsValid) {
-      return {
-        statusCode: 400,
-        body: {
-          error: [
-            new InvalidParamError('Email')
-          ]
+      const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!emailIsValid) {
+        return {
+          statusCode: 400,
+          body: {
+            error: [
+              new InvalidParamError('Email')
+            ]
+          }
         }
+      }
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
       }
     }
   }
