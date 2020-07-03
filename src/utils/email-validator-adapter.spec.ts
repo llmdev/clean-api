4c3337e3f543cd738a8efd17da7b03682e694ee5
Lucas Lopes de Moura <lucaslopes.dev@gmail.com>
2020-07-03 01:02:22 -0300

    feat: add library validator to valid email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,41 @@
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 
 describe('EmailValidator Adapter', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('valid_email@email.com')
+    expect(isValid).toBe(true)
+  })
+
+  test('Should call validator function with email', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('valid_email@email.com')
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+
+    expect(isValid).toBe(true)
+    expect(isEmailSpy).toHaveBeenCalledWith('valid_email@email.com')
+    expect(isEmailSpy).toHaveBeenCalledTimes(1)
   })
 })
