205cbb674ff91c8c75ddcccb19d9f81ee3fa0641
Lucas Lopes de Moura <lucaslopes.dev@gmail.com>
2020-07-04 01:05:13 -0300

    refactor: makeSut bcryptAdapter

--- src/infra/cripthography/bcrypt-adapter.spec.ts
+++ src/infra/cripthography/bcrypt-adapter.spec.ts
@@ -1,11 +1,16 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+const encryptSalt = 12
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
     return await new Promise(resolve => resolve('hash_value'))
   }
 }))
+
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(encryptSalt)
+}
 
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct value', async () => {
@@ -17,8 +22,7 @@ describe('Bcrypt Adapter', () => {
   })
 
   test('Should return a hash on success', async () => {
-    const encryptSalt = 12
-    const sut = new BcryptAdapter(encryptSalt)
+    const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash_value')
   })
