c39c5cf8635a00792f19f6a997028e05c22d65c5
Lucas Lopes de Moura <lucaslopes.dev@gmail.com>
2020-07-04 01:02:15 -0300

    test: ensure return bcrypt hash values on success

--- src/infra/cripthography/bcrypt-adapter.spec.ts
+++ src/infra/cripthography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('hash_value'))
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct value', async () => {
     const encryptSalt = 12
@@ -8,5 +14,12 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', encryptSalt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const encryptSalt = 12
+    const sut = new BcryptAdapter(encryptSalt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash_value')
   })
 })
