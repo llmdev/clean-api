1d282ac6b1a768997a625bddbf69534c5b0586fc
Lucas Lopes de Moura <lucaslopes.dev@gmail.com>
2020-07-05 15:19:47 -0300

    refactor: title test body parser

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -2,7 +2,7 @@ import app from './../config/app'
 import request from 'supertest'
 
 describe('Body Parser Middleware', () => {
-  test('Should', async () => {
+  test('Should parse json with body parser', async () => {
     app.post('/test_body_parser', (req, res) => {
       res.send(req.body)
     })
