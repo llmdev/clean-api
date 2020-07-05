a33120e0acc718d37af32b6db743784ece6f4460
Lucas Lopes de Moura <lucaslopes.dev@gmail.com>
2020-07-05 16:22:44 -0300

    test: middleware default response content-type json

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('CONTENT-TYPE', () => {
+  test('Should return default content-type json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  test('Should return default content-type xml whenf forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
